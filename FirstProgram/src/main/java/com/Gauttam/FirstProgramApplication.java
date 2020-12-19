bba39cff7b9b4e96e6b0dbb51a39f09e7e6dd1d6
Gauttam Sonkamble <46339795+Gauttamsonkamble@users.noreply.github.com>
2020-12-19 22:10:55 +0530

    Add files via upload

---
+++ FirstProgram/src/main/java/com/Gauttam/FirstProgramApplication.java
@@ -0,0 +1,15 @@
+package com.Gauttam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProgramApplication.class, args);
+		
+		System.out.println("Hello World");
+	}
+
+}
