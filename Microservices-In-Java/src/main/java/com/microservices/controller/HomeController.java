c1f3bbef67e9db8139f2680d1b2b476d0e9633d6
Gauttam Sonkamble <46339795+Gauttamsonkamble@users.noreply.github.com>
2020-12-20 19:47:44 +0530

    Add files via upload

---
+++ Microservices-In-Java/src/main/java/com/microservices/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.microservices.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController 
+{
+	@RequestMapping("/home")
+	public String greet()
+	{
+		return "Hello World";
+	}
+	
+
+}
