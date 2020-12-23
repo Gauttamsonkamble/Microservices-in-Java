4df011eae54718c7f23743c75281e24e022db049
Gauttam Sonkamble <46339795+Gauttamsonkamble@users.noreply.github.com>
2020-12-23 14:21:33 +0530

    Add files via upload

--- Microservices-In-Java/src/main/java/com/microservices/controller/HomeController.java
+++ Microservices-In-Java/src/main/java/com/microservices/controller/HomeController.java
@@ -1,7 +1,13 @@
 package com.microservices.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.microservices.model.Items;
 
 @RestController
 public class HomeController 
@@ -11,6 +17,20 @@ public class HomeController
 	{
 		return "Hello World";
 	}
+	
+	@RequestMapping("/getItems")
+	public List<Items> getItems()
+	{
+		List list = new ArrayList<>();
+		
+			list.add(101);
+			list.add("iphone");
+			list.add(50000);
+			
+			return list;
+	}
+	
+	
 	
 
 }
